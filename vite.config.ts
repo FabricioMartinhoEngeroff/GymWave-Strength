e60ab3c51efb7844dc86801216749fe7b00efd70
fabricio <fa.engeroff1996@gmail.com>
2025-12-22 14:28:50 -0300

    configura HTTPS com certificado SSL no Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import fs from "fs";
 
 export default defineConfig({
-  base: '/',
   plugins: [react()],
-})
+  server: {
+    https: {
+      key: fs.readFileSync("./server.key"),
+      cert: fs.readFileSync("./server.crt"),
+    },
+    port: 5173,
+  },
+});
