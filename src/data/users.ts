ff2fba54b33c1e2addc16035dea18d7f1661fa8d
Fabricio Matinho Engeroff <fa.engeroff1996@gmail.com>
2026-08-04 16:50:07 -0300

    atualizei o user

--- src/data/users.ts
+++ src/data/users.ts
@@ -1,6 +1,6 @@
 export const HARDCODED_USERS = [
   { email: "treino@gmail.com", password: "@Treino123", name: "Fabricio" },
-  { email: "amanda@treino.com", password: "@TreinoAmanda", name: "Amanda" },
+  { email: "amanda@treino.com", password: "@TreinoAmanda123", name: "Amanda" },
 ] as const;
 
 export type AppUser = (typeof HARDCODED_USERS)[number];
